ad0afcf4f33a96966e21f8ffa3c5caa36c0a0886
Moses <kamuirumoses360@gmail.com>
2022-10-28 10:19:31 +0300

    m

--- app/Http/Controllers/MpesaResponseController.php
+++ app/Http/Controllers/MpesaResponseController.php
@@ -9,10 +9,9 @@ use Illuminate\Support\Facades\Log;
 class MpesaResponseController extends Controller
 {
     public function mpesaSTKPush(Request $request){
-        $response = json_decode($request->getContent());
+        $response = json_decode($request->all());
         Log::info(json_encode($response));
-        dd($response);
-        $amount = $response['Body']['stkCallback']['CallbackMetadata']['Item[0]']['Value'];
+        $amount = response->Body->stkCallback->CallbackMetadata->Item[0]->Value;
         $transaction_id = $response->Body->stkCallback->CallbackMetadata->Item[1]->Value;
         $date = $response->Body->stkCallback->CallbackMetadata->Item[2]->Value;
         $phone = $response->Body->stkCallback->CallbackMetadata->Item[3]->Value;
