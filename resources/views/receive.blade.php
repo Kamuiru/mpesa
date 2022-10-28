62b59d02876bbfcc833e3f7607a772784873469e
Moses <kamuirumoses360@gmail.com>
2022-10-28 10:50:18 +0300

    m

--- resources/views/receive.blade.php
+++ resources/views/receive.blade.php
@@ -3,6 +3,6 @@
 @php
 $response = file_get_contents("php://input");
 $response = json_decode($response, true);
-error_log(print_r($response, true, 0));
+error_log(print_r($response, true));
 @endphp
 @endsection
